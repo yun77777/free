13c449f94a8cb98b8a7642e00ce91f892aa8dfa0
yun77777 <victoriousyk@naver.com>
2022-09-12 18:39:56 +0900

    login(check email -> check password -> make token)

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const config = require('./config/key')
 const { User } = require('./models/User')
 
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // application/json
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI, {
         useNewUrlParser: true,
@@ -29,11 +31,43 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('hi'))
 
 app.post('/register', (req, res) => {
+    console.log('/register req.body -> ', req.body)
+
     const user = new User(req.body)
-    user.save((err, userInfo) => {
+    user.save((err, user) => {
         if (err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    console.log('/login req.body -> ', req.body)
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+        console.log('User.findOne -> ', user)
+
+        if (!user) {
+            return res.json({
+                loginSccess: false,
+                message: "email doesn't exist"
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            console.log('user.comparePassword -> ', isMatch)
+
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "wrong password" })
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err)
+
+                // save token in cookie / localstorage / session ...
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id.toHexString() })
+            })
         })
     })
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -14,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 5
+        // maxlength: 5
     },
     lastname: {
         type: String,
@@ -43,12 +44,38 @@ userSchema.pre('save', function(next) {
 
             bcrypt.hash(user.password, salt, function(err, hash) {
                 if (err) return next(err)
+                console.log('user.password -> ', this.password)
                 user.password = hash;
+                console.log('hash -> ', this.password)
                 next()
             })
         })
+    } else {
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    console.log('comparePassword() plain-> ', plainPassword)
+    console.log('comparePassword() password-> ', this.password)
+    bcrypt.compare(String(plainPassword), this.password, function(err, isMatch) {
+        console.log('err->', err)
+
+        if (err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+        // user._id + 'secretToken' = token
+    user.token = token
+    user.save(function(err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
