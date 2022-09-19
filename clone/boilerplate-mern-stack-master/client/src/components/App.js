55896978d860c50fc71d15d0e0fd232707327a47
yun77777 <victoriousyk@naver.com>
2022-09-19 12:12:03 +0900

    movie details

--- clone/boilerplate-mern-stack-master/client/src/components/App.js
+++ clone/boilerplate-mern-stack-master/client/src/components/App.js
@@ -5,8 +5,10 @@ import Auth from "../hoc/auth";
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import MovieDetail from "./views/MovieDetail/MovieDetail.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
+
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -21,6 +23,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
         </Switch>
       </div>
       <Footer />
---
+++ clone/boilerplate-mern-stack-master/client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config'
+import MainImage from '../LandingPage/Sections/MainImage'
+import MovieInfo from './Sections/MovieInfo'
+import GridCards from '../commons/GridCards'
+import { Row } from 'antd'
+
+function MovieDetail(props) {
+    let movieId = props.match.params.movieId
+    const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
+
+    // when DOM is loaded, run this
+    useEffect(()=>{
+        console.log('props.match -> ', props.match)
+
+        const endpointCrew =`${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+        const endpointInfo =`${API_URL}movie/${movieId}?api_key=${API_KEY}`
+
+        fetch(endpointInfo)
+        .then(response=>response.json())
+        .then(response=>{
+            console.log(response)
+            setMovie(response)
+        })
+
+        fetch(endpointCrew)
+        .then(response=>response.json())
+        .then(response=>{
+            console.log(response)
+            setCasts(response.cast)
+        })
+    }, [])
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle)
+    }
+
+  return (
+      <div>
+          {/* Header */}
+          { Movie && 
+                <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                title={Movie.original_title}
+                text={Movie.overview}
+                />
+            }
+
+          {/* Body */}
+          <div style={{width: '85%', margin: '1rem auto'}}>
+            {/* Movie Info */}
+            <MovieInfo 
+                movie={Movie}
+            />
+            <br/>
+
+            {/* Actors Grid */}
+            <div style={{display:'flex', justifyContent:'center', margin:'2rem'}}>
+                <button onClick={toggleActorView}>Toggle Actor View</button>
+            </div>
+
+            {ActorToggle && 
+            <Row gutter={[16,16]}>
+            {Casts && Casts.map((casts, index) => (
+               <React.Fragment key={index}>
+                   <GridCards 
+                   image={casts.profile_path ?
+                   `${IMAGE_BASE_URL}w500${casts.profile_path}`:null}
+                   chracterName={casts.name}
+                   />
+               </React.Fragment>
+            ))}
+            </Row>
+            }
+            
+          </div>
+      </div>
+  )
+}
+
+export default MovieDetail
